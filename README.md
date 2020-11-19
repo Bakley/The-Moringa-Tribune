51b1b2a5871b92b0acc1c4711c417b6f0a2437ab
Bakley <barclaykoin@gmail.com>
2020-11-19 10:26:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+## Development set up
+
+-   Check that python 3.6.x is installed:
+
+    ```
+    python --version
+    >> Python 3.6.x
+    ```
+
+-   Install pipenv:
+
+    ```
+    pip3 install pipenv
+    ```
+
+-   Check pipenv is installed:
+    ```
+    pipenv --version
+    >> pipenv, version 2018.11.26
+    ```
+    - Create  virtual environment
+    ```
+    pipenv --python 3.6
+
+    ```
+- Turn off a virtual environment  
+    ```
+    exit
+    ```
+
+- Spawn a shell in a virtual environment
+    ```
+    pipenv shell
+    ```
+- Run migrations then run your server
+
--- tribune/news/urls.py
+++ tribune/news/urls.py
@@ -1,18 +1,11 @@
 from django.urls import include, path
 
-# from django_registration.backends.one_step.views import RegistrationView
-
-
 from . import views
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('today', views.news_today, name='news_today'),
     path('tinymce/', include('tinymce.urls')),
-    # path(
-    #     'accounts/register/',
-    #     RegistrationView.as_view(success_url='/profile/')
-    # ),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
